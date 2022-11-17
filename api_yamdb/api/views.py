aa9ed39aade26e806a2b38d66253dd28c4973792
LunarBirdMYT <pav.mgk50@mail.ru>
2022-11-17 10:29:56 +0300

    Добавлен импорт декораторов.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -4,6 +4,7 @@ from django.core.mail import send_mail
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
